348e36b5596dcee60f596204cc27904eebc4e016
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-19 14:15:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 $servername = "localhost";
 $username = "projecto_homework3";
 $password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$dbname = "sanaoucr_homework3-MIS4013";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
